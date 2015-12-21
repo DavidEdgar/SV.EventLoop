02e56cbba8d8aad0f3c7ebf216f3f869542ba5dd
David Edgar <david.edgar@salmat.com.au>
2015-12-22 09:52:22 +1100

    Timer is basically a GetSleep loop, with a defined sleep time at the end of the processing

---
+++ Timer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SV.EventLoop
+{
+    public class Timer<T> : IEventLoop<T>
+    {
+        private readonly int _sleepTime;
+        public Func<T> _subjectGetter;
+
+        public Timer(Func<T> subjectGetter, int sleepTime)
+        {
+            this._subjectGetter = subjectGetter;
+            this._sleepTime = sleepTime;
+        } 
+
+        public void Subscribe(Action<T> processFunc)
+        {
+            while (true)
+            {
+                T data = _subjectGetter();
+                processFunc(data);
+                Thread.Sleep(this._sleepTime);
+            }
+        }
+    }
+}
